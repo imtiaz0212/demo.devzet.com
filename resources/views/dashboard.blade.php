c1b6f2cc3bbf50723b51035480fba75934f6b783
Imtiaz <imitaz0212@gmail.com>
2025-03-13 15:04:29 +0600

    dashboard test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,4 +3,4 @@
 <h3>This is a test</h3>
 <h4>This is a test</h4>
 <h5>This is a test</h5>
-<h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, quis, explicabo in soluta debitis dolor dicta officia ducimus nulla placeat repellendus sequi quasi illum maiores quo velit enim! Quibusdam, facere.</h6>
+<h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, quis, explicabo in soluta debitis dolor</h6>
