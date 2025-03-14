7e33acae96c859097ba907a79e4d59b5512d297d
Imtiaz <imitaz0212@gmail.com>
2025-03-14 16:11:26 +0600

    update data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,4 @@
 <h1>This is a test</h1>
 <h2>This is a test</h2>
 <h3>This is a test</h3>
-<h4>This is a test</h4>
-<h5>This is a test</h5>
-<h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, quis, explicabo in soluta debitis dolor</h6>
-
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, fuga! Ex quaerat nam deserunt adipisci eum ullam est non natus maiores, provident nostrum saepe voluptas suscipit doloribus quas sint ab?
